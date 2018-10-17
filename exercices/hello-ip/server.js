4641438e2226c44d496417db814743c05b8c3f00
josue <josue@hackages.io>
2018-10-17 21:01:03 +0200

    added env variable

--- exercices/hello-ip/server.js
+++ exercices/hello-ip/server.js
@@ -10,7 +10,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n'+ip.address());
+  res.send('Hello world\n'+ip.address()+"\n"+process.env);
 });
 
 app.listen(PORT, HOST);
